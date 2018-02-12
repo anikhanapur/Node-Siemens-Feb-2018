3f10d536efae34bb5a00aace12259bdd186b2e31
Magesh <tkmagesh77@gmail.com>
2018-02-12 16:34:14 +0530

    calculator html page for post

---
+++ 07-web-app-server/server.js
@@ -0,0 +1,42 @@
+var http = require('http'),
+	path = require('path'),
+	url = require('url'),
+	querystring = require('querystring'),
+	fs = require('fs'),
+	calculator = require('./calculator');
+
+var staticExtns = ['.html', '.css', '.js', '.xml', '.ico', '.png', '.json'];
+
+function isStatic(resourceName){
+	return staticExtns.indexOf(path.extname(resourceName)) !== -1;
+}
+
+var server = http.createServer(function(req, res){
+	var urlObj = url.parse(req.url === '/'? '/index.html' : req.url ),
+		resourceName = urlObj.pathname;
+	if (isStatic(resourceName)){
+		var resourcePath = path.join(__dirname, resourceName);
+		if (!fs.existsSync(resourcePath)){
+			res.statusCode = 404;
+			res.end();
+			return;
+		}
+		fs.createReadStream(resourcePath).pipe(res);
+	} else if (resourceName === '/calculator'){
+		var data = querystring.parse(urlObj.query);
+		var op = data.op,
+			x = parseInt(data.x, 10),
+			y = parseInt(data.y, 10),
+			result = calculator[op](x,y);
+
+		res.write(result.toString());
+		res.end();
+	} else {
+		res.statusCode = 404;
+		res.end();
+		return;
+	}
+	
+});
+
+server.listen(8080);
